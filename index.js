f0afdae0b709fd23c610481e2fc0d66698bae92b
Krishap-s <krishap.s.sreenivasan@gmail.com>
2022-01-14 15:44:41 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+var port = process.env.PORT || 3000
+
+var corsConfig = {
+    origin:'*'
+}
+
+var app = express()
+
+app.use(cors(corsConfig))
+
+app.listen(port,() => {
+    console.log(`Server listening on ${port}`)
+})
